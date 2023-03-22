1ddd1702b965a29f2ba44b21394c5aee32ec9c0d
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2023-03-21 20:50:41 -0700

    Get and public folder access

--- server.js
+++ server.js
@@ -2,3 +2,14 @@ console.log('Uhhhh Hellooo??')
 
 const express = require('express')
 const app = express()
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.send(__dirname + './public/index.html')
+})
+
+
+
+app.listen(3000, () => {
+  console.log('listening on 3000')
+})
