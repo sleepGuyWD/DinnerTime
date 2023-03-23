796cf8bb708e8e6f88ee5acd50f03363262b1f07
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2023-03-22 23:45:52 -0700

    ENV, MongoDB Connected

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ console.log('Uhhhh Hellooo??')
 const express = require('express')
 const app = express()
 app.use(express.static('public'))
+require('dotenv').config()
+
+let db,
+    dbConnectionStr = DB_STRING,
+    dbName = 'dinner'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
 
 app.get('/', (req, res) => {
   res.send(__dirname + './public/index.html')
